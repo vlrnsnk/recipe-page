c1630bafd3b87657b5a4c544a24d03f0abdfcea3
Valeriy Anysenko <168164564+vlrnsnk@users.noreply.github.com>
2026-04-27 23:46:45 -0500

    chore: add auto browser start when run dev

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/recipe-page/' : '/',
+
+  server: {
+    open: true,
+    port: 5173,
+    strictPort: true,
+  },
+
   build: {
     outDir: 'dist',
     emptyOutDir: true,
   },
-  base: mode === 'production' ? '/recipe-page/' : '/',
 }));
